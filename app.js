a047189c3fb1ec83be0dc98e8093356b4bdbdb79
KajalMishra-29 <kajalmishra0829@gmail.com>
2025-02-04 01:09:20 +0530

    styling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/CozyCorners");
@@ -19,9 +20,10 @@ main()
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
+app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(methodOverride("_method"));
 
